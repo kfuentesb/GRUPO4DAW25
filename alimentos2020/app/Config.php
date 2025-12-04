9954f54945d73372433e541a7e5247c08e2cfff4
Abeagom <beas.gomez.alberto@iescamas.es>
2025-12-04 12:40:52 +0100

    Archivos integrante 3

---
+++ alimentos2020/app/Config.php
@@ -0,0 +1,11 @@
+<?php
+
+ class Config
+ {
+     static public $mvc_bd_hostname = "mysql-controlversionesdespliegue.alwaysdata.net";
+     static public $mvc_bd_nombre   = "controlversionesdespliegue_alimentos42025";
+     static public $mvc_bd_usuario  = "controlVersionesDespliegue";
+     static public $mvc_bd_clave    = "8%*uCy26ao62";
+     static public $mvc_vis_css     = "estilo.css";
+ }
+?>
