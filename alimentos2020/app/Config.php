057e7820f18b1ea10631f64878b026b22d1b0866
Abeagom <beas.gomez.alberto@iescamas.es>
2025-12-05 12:05:10 +0100

    Cambios en Config.php

--- alimentos2020/app/Config.php
+++ alimentos2020/app/Config.php
@@ -4,8 +4,8 @@
  {
      static public $mvc_bd_hostname = "mysql-controlversionesdespliegue.alwaysdata.net";
      static public $mvc_bd_nombre   = "controlversionesdespliegue_alimentos42025";
-     static public $mvc_bd_usuario  = "controlVersionesDespliegue";
-     static public $mvc_bd_clave    = "8%*uCy26ao62";
+     static public $mvc_bd_usuario  = "444421";
+     static public $mvc_bd_clave    = "grupo4Despliegue";
      static public $mvc_vis_css     = "estilo.css";
  }
 ?>
